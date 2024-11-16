980a031c275208ca572a185c5d130c3060df45cf
Amanda Martins <47390004+mandimartins@users.noreply.github.com>
2024-11-16 17:39:21 -0300

    feat:add Menu and MenuItem tables (#9)

---
+++ Restaurant/Data/EntityTypeConfiguration/MenuItemConfig.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Restaurant.Data.Models;
+
+namespace Restaurant.Data.EntityTypeConfiguration
+{
+    public class MenuItemConfig : IEntityTypeConfiguration<MenuItem>
+    {
+        public void Configure(EntityTypeBuilder<MenuItem> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.IdMenu)
+                 .IsRequired();
+
+            builder.Property(x => x.IdProduct) 
+                .IsRequired();
+
+            builder.HasOne(x => x.Product)
+                .WithMany()
+                .HasForeignKey(x => x.IdProduct)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(x => x.Menu)
+                .WithMany()
+                .HasForeignKey(x => x.IdMenu)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
